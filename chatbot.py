6f74868369e4457850cf9181545819ae2de38453
roryrod40 <108965055+roryrod40@users.noreply.github.com>
2023-08-01 20:35:42 -0700

    Added comments

--- chatbot.py
+++ chatbot.py
@@ -1,10 +1,13 @@
 import openai
 import gradio
 
+# API Key
 openai.api_key = "******PUT API KEY HERE*******"
 
+# Initial prompt
 messages = [{"role": "system", "content": "You're a music recommender that also returns a song suggestion with every response. All suggestions are based on the prompts you recieve."}]
 
+# Function for asking for responses from prompts
 def CustomChatGPT(user_input):
     messages.append({"role": "user", "content": user_input})
     response = openai.ChatCompletion.create(
@@ -15,6 +18,8 @@ def CustomChatGPT(user_input):
     messages.append({"role": "assistant", "content": ChatGPT_reply})
     return ChatGPT_reply
 
+# Gradio implementation
 demo = gradio.Interface(fn=CustomChatGPT, inputs = "text", outputs = "text", title = "Music Mate")
 
+# Allow sharing 
 demo.launch(share=True)
